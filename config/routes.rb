ddde814208b4643e9c374b28b37a12ab8611e13c
crackmoon <izizvanezza@hotmail.com>
2013-10-08 04:44:40 +0000

    Se agrego index

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Manytomany::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'index#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  #get 'index_controller/' => 'index#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
